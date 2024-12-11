c5f0aded2bf02cb4adb1c9215c2cf0f35cafa529
katheGut <katheringutierrez79@gmail.com>
2024-12-10 23:43:58 -0500

    .

---
+++ AdmibolsilloN/Models/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Admibolsillo.Models.ViewModels
+{
+    public class RegistroViewModel
+    {
+        [Key]
+        public int? Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        public required string Nombre { get; set; }
+
+        [Display(Name = "Apellido")]
+        public string? Apellido { get; set; }
+
+        [Range(0, 120, ErrorMessage = "La edad debe estar entre 0 y 120 años.")]
+        [Display(Name = "Edad")]
+        public int? Edad { get; set; }
+
+        [Display(Name = "Sexo")]
+        public string? Sexo { get; set; }
+
+        [Required]
+        [Display(Name = "Correo Electrónico")]
+        [EmailAddress(ErrorMessage = "Ingrese un correo válido.")]
+        public required string Correo { get; set; }
+
+        [Display(Name = "Celular")]
+        [Phone(ErrorMessage = "Ingrese un número de celular válido.")]
+        public int? Celular { get; set; }
+
+        [Required]
+        [Display(Name = "Documento")]
+        public required int Documento { get; set; }
+
+        [Display(Name = "Tipo de Usuario")]
+        public string? IdTipoUsuario { get; set; }
+
+        [Required]
+        [Display(Name = "Contraseña")]
+        [StringLength(20, ErrorMessage = "La contraseña no puede tener más de 20 caracteres.")]
+        public required string Contrasena { get; set; }
+
+        [Display(Name = "Foto de Perfil")]
+        public string? Foto { get; set; }
+    }
+}
--- AdmibolsilloN/obj/Debug/net8.0/AdmibolsilloN.AssemblyInfo.cs
+++ AdmibolsilloN/obj/Debug/net8.0/AdmibolsilloN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdmibolsilloN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d895ba45f902a54a210d6ae5df0670900d63d943")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0e2a50cc52838a88d2633ca11efe62a1deb0b9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdmibolsilloN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdmibolsilloN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
